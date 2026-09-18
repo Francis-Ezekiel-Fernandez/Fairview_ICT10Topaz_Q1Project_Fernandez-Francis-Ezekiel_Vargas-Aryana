dc58ab5d4096ede0d8d5f1317d18d26e8ab7d6b2
Francis-Ezekiel-Fernandez <fernandez.fe@obmontessori.edu.ph>
2026-09-18 21:44:37 +0800

    1

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 from pyscript import document, display
 
 def create_order(e):
-    document.getElementById("output2").innerHTML = "" # clears previous output
-    prod1= document.getElementById("item1")
-    # Calculate
-    subtotal = float(prod1.value) * prod1.checked
-    size = document.querySelector('input[name="size"]:checked')
-    size_price = float(size.value)
-    grandtotal = subtotal + size_price
-    display(grandtotal, target="output2")
+    document.getElementById("output1").innerHTML = ""  # Clear previous output
+    document.getElementById("output3").innerHTML = ""  # Clear previous output
+    document.getElementById("output2").innerHTML = ""  # Clear previous output
+
+    subtotal = 0
+
+    prod1 = document.getElementById("item1")
+    prod2 = document.getElementById("item2")
+    prod3 = document.getElementById("item3")
+    prod4 = document.getElementById("item4")
+    prod5 = document.getElementById("item5")
+    prod6 = document.getElementById("item6")
+    if prod1.checked:
+        subtotal = subtotal + float(prod1.value)
+    if prod2.checked:
+        subtotal = subtotal + float(prod2.value)
+    if prod3.checked:
+        subtotal = subtotal + float(prod3.value)
+    if prod4.checked:
+        subtotal = subtotal + float(prod4.value)
+    if prod5.checked:
+        subtotal = subtotal + float(prod5.value)
+    if prod6.checked:
+        subtotal = subtotal + float(prod6.value)
+
+    pesos = "PhP"
+        
+    display(f"{pesos}" + f"{subtotal}", target="output1")
+
+    tax = subtotal * 0.12
+
+    display(f"{pesos}" + f"{tax}", target="output2")
+
+    grandtotal = subtotal + tax
+
+    display(f"{pesos}" + f"{grandtotal}", target="output3")
